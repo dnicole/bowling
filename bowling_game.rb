7684b618486ba44d115bab588303df2affec6f24
nicole pank <nicole.pank@gmail.com>
2013-09-12 10:11:08 -0500

    add roll and score methos; add comments

--- bowling_game.rb
+++ bowling_game.rb
@@ -1,6 +1,23 @@
 class BowlingGame
   attr_accessor :rolls
+
+  # Instantiate a new bowling game.
+  #
+  # rolls   - The array listing all the 
+  #           rolls that have been made in the game
+  #
+  # Returns a new game object.
+  
   def initialize()
     @rolls = []
   end
+
+  def roll(pins)
+
+  end
+
+  def score
+    0
+  end
+
 end
