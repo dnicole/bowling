3819bf8403d4649dcc0f1342f9f0b2c23c7a8f88
nicole pank <nicole.pank@gmail.com>
2013-09-12 10:55:53 -0500

    fix scoring to allow for players that don't suck at bowling.

--- bowling_game.rb
+++ bowling_game.rb
@@ -24,8 +24,33 @@ class BowlingGame
   end
 
   # Returns the Integer score for this game.
+  #
+  # Scores 10 + the number of pins in the next roll
+  # for a spare
+  #
+  # Scores based on pins knocked down per frame if not a spare/strike
+  #
+  #
+
   def score
-    @rolls.reduce(:+)
+    total_score = 0
+    current_roll = 0
+
+    while current_roll < @rolls.size - 1
+      roll = @rolls[current_roll]
+      next_roll = @rolls[current_roll + 1]
+
+      if roll + next_roll == 10
+        total_score += 10 + @rolls[current_roll + 2]
+      else
+        total_score += roll + next_roll
+      end
+
+      current_roll += 2
+    end
+
+    return total_score
   end
+    #@rolls.reduce(:+) - quick way of summing all numbers in an array
 
 end
