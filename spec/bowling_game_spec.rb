0f23df2bedc2e3f16acbddb89735e7101d3eaa2e
nicole pank <nicole.pank@gmail.com>
2013-09-12 10:31:50 -0500

    add context

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -13,15 +13,19 @@ describe BowlingGame do
   end
 
   describe '#score' do
-    it 'scores the game a 0 when only gutterballs are thrown' do
-      20.times { game.roll(0) }
+    context 'when only gutterballs are thrown' do
+      it 'scores the game a 0' do
+        20.times { game.roll(0) }
 
-      expect(game.score).to eq(0)
+        expect(game.score).to eq(0)
+      end
     end
-    it 'scores the game as 20 when one pin is knocked down on each roll' do
-      20.times { game.roll(1) }
+    context 'when one pin is knocked down on each roll' do
+      it 'scores the game as 20' do
+        20.times { game.roll(1) }
 
-      expect(game.score).to eq(20)
+        expect(game.score).to eq(20)
+      end
     end
   end  
 end 
